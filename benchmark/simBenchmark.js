bf23c7bdd0623bceefe4b7f5248bd5b9488536d2
John Lavelle <jlavelle604@gmail.com>
2017-06-07 21:48:12 -0400

    Fix bug where successive trials used the same cells

--- benchmark/simBenchmark.js
+++ benchmark/simBenchmark.js
@@ -1,29 +1,29 @@
+import { range } from 'ramda'
 import { game } from '../server/life/life'
 import { ArrayGrid, SetGrid } from '../shared/grid'
 
 function SimpleLifeBenchmark(gridConstructor, size, maxGens, trials) {
     const grid = gridConstructor(size)
-    let cells = grid.randomFill(0.5)
     const sim = game(grid)(size)
-
+    const randomGrids = range(0, trials).map((_) => grid.randomFill(0.5))
     const now = Date.now()
-    for (let i = 0; i < trials; i++) {
-        for (let j = 0; j < maxGens; j++) {
-            cells = sim(cells)
+    randomGrids.forEach(grid => {
+        for (let i = 0; i < maxGens; i++) {
+            grid = sim(grid)
         }
-    }
+    })
     return (Date.now() - now)
 }
 
 function runGridBenchmarks() {
     const generations = 1
-    const size = 1000
-    const trials = 1
+    const size = 100
+    const trials = 1000
     const types = [ArrayGrid]
     console.log(`Running Benchmarks with size=${size}, generations=${generations}, trials=${trials}`)
     types.forEach(type => {
         const time = SimpleLifeBenchmark(type, size, generations, trials)
-        console.log(`${type(size).toString()}: ${time / 1000}s`)
+        console.log(`${type(size).toString()}: ${time / trials}ms average per trial`)
     })
 }
 

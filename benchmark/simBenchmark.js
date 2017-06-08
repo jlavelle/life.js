37800a5fc6b05db74d54ba87a55e988e6263c2f6
John Lavelle <jlavelle604@gmail.com>
2017-06-07 20:36:12 -0400

    Run n trials

--- benchmark/simBenchmark.js
+++ benchmark/simBenchmark.js
@@ -1,25 +1,28 @@
 import { game } from '../server/life/life'
 import { ArrayGrid, SetGrid } from '../shared/grid'
 
-function SimpleLifeBenchmark(gridConstructor, size, maxGens) {
+function SimpleLifeBenchmark(gridConstructor, size, maxGens, trials) {
     const grid = gridConstructor(size)
     let cells = grid.randomFill(0.5)
     const sim = game(grid)(size)
 
     const now = Date.now()
-    for (let i = 0; i < maxGens; i++) {
-        cells = sim(cells)
+    for (let i = 0; i < trials; i++) {
+        for (let j = 0; j < maxGens; j++) {
+            cells = sim(cells)
+        }
     }
     return (Date.now() - now)
 }
 
 function runGridBenchmarks() {
-    const generations = 100
-    const size = 100
+    const generations = 1
+    const size = 1000
+    const trials = 1
     const types = [ArrayGrid, SetGrid]
-    console.log(`Running Benchmarks with size=${size}, generations=${generations}`)
+    console.log(`Running Benchmarks with size=${size}, generations=${generations}, trials=${trials}`)
     types.forEach(type => {
-        const time = SimpleLifeBenchmark(type, size, generations)
+        const time = SimpleLifeBenchmark(type, size, generations, trials)
         console.log(`${type(size).toString()}: ${time / 1000}s`)
     })
 }

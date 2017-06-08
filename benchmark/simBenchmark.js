8e18376dd898c1ca208044a2a0a1459d7029f7a1
John Lavelle <jlavelle604@gmail.com>
2017-06-08 00:21:33 -0400

    hardcoded GoL, don't destructure in liveNeighbors

--- benchmark/simBenchmark.js
+++ benchmark/simBenchmark.js
@@ -1,4 +1,4 @@
-import { range } from 'ramda'
+import { range, sum } from 'ramda'
 import { game } from '../server/life/life'
 import { ArrayGrid, SetGrid } from '../shared/grid'
 
@@ -6,24 +6,26 @@ function SimpleLifeBenchmark(gridConstructor, size, maxGens, trials) {
     const grid = gridConstructor(size)
     const sim = game(grid)(size)
     const randomGrids = range(0, trials).map((_) => grid.randomFill(0.5))
-    const now = Date.now()
+    const results = []
     randomGrids.forEach(grid => {
+        const now = Date.now()
         for (let i = 0; i < maxGens; i++) {
             grid = sim(grid)
         }
+        results.push(Date.now() - now)
     })
-    return (Date.now() - now)
+    return sum(results) / results.length
 }
 
 function runGridBenchmarks() {
     const generations = 1
-    const size = 100
-    const trials = 1000
+    const size = 1000
+    const trials = 10
     const types = [ArrayGrid]
     console.log(`Running Benchmarks with size=${size}, generations=${generations}, trials=${trials}`)
     types.forEach(type => {
         const time = SimpleLifeBenchmark(type, size, generations, trials)
-        console.log(`${type(size).toString()}: ${time / trials}ms average per trial`)
+        console.log(`${type(size).toString()}: ${time}ms average per trial`)
     })
 }
 
--- server/life/life.js
+++ server/life/life.js
@@ -1,5 +1,6 @@
 import { Point, getCoordinatesOfIndex, getIndexOfCoordinates } from '../../shared/coordinates'
 import { SetGrid } from '../../shared/grid'
+import { sequence } from 'ramda'
 
 function Ruleset(born, survives) {
     return (alive, neighbors) => {
@@ -17,46 +18,58 @@ function Ruleset(born, survives) {
 const gameOfLife = Ruleset([3], [2, 3])
 const highLife = Ruleset([3, 6], [2, 3])
 
+function precomputeStates() {
+
+}
+
+function fastLife(alive, neighbors) {
+    const livingN = neighbors.length
+    if(alive) {
+        return livingN === 2 || livingN === 3
+    } else {
+        return livingN === 3
+    }
+}
+
 export function game(gridType) {
     return function (n) {
         const getCoordinates = getCoordinatesOfIndex(n)
-            const getIndex = getIndexOfCoordinates(n)
+        const getIndex = getIndexOfCoordinates(n)
 
-            function liveNeighbors({ x, y }, cells) {
-                const neighbors = [
-                    [x - 1, y - 1],
-                    [x, y - 1],
-                    [x + 1, y - 1],
-                    [x - 1, y],
-                    [x + 1, y],
-                    [x - 1, y + 1],
-                    [x, y + 1],
-                    [x + 1, y + 1]
-                ]
-                return neighbors.filter(pair => {
-                    const [x, y] = pair
-                    return inBounds(x, y) && cells.alive(n * x + y)
-                }).length
-            }
+        function liveNeighbors({ x, y }, cells) {
+            const neighbors = [
+                [x - 1, y - 1],
+                [x, y - 1],
+                [x + 1, y - 1],
+                [x - 1, y],
+                [x + 1, y],
+                [x - 1, y + 1],
+                [x, y + 1],
+                [x + 1, y + 1]
+            ]
+            return neighbors.filter(pair => {
+                return cells.alive(n * pair[0] + pair[1]) && inBounds(pair[0], pair[1])
+            }).length
+        }
 
-            function inBounds(x, y) {
-                const size = n * n
-                return (x >= 0 && x < size) && (y >= 0 && y < size)
-            }
+        function inBounds(x, y) {
+            const size = n * n
+            return (x >= 0 && x < size) && (y >= 0 && y < size)
+        }
 
-            function checkForLife(cellState, point, cells) {
-                const neighbors = liveNeighbors(point, cells)
-                return gameOfLife(cellState, neighbors)
-            }
+        function checkForLife(cellState, point, cells) {
+            const neighbors = liveNeighbors(point, cells)
+            return fastLife(cellState, neighbors)
+        }
 
-            return function update(grid) {
-                const nextGrid = gridType()
-                for (var i = 0; i < n * n; i++) {
-                    const point = getCoordinates(i)
-                    const nextState = checkForLife(grid.alive(i), point, grid)
-                    if (nextState) nextGrid.birth(i)
-                }
-                return nextGrid
+        return function update(grid) {
+            const nextGrid = gridType()
+            for (var i = 0; i < n * n; i++) {
+                const point = getCoordinates(i)
+                const nextState = checkForLife(grid.alive(i), point, grid)
+                if (nextState) nextGrid.birth(i)
             }
+            return nextGrid
+        }
     }
 }
